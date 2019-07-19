4e3c0b60780f66e461e1a8f6c0be86a0cec7fb81
Weijing Jay Lin <weijingjaylin@gmail.com>
2019-07-18 18:04:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 console.log('hello, world! by global node cli sample');
-console.log('https://github.dev.xero.com/jay-lin/global-node-cli-sample');
+console.log('https://github.com/dotku/global-node-cli-sample');
 
